2708e4596e17005c38a31a89a7025c7f66e52fd5
SailorSnoW <snxwin@pm.me>
2022-10-02 01:22:19 +0200

    added MaxNameLength type

--- src/types.rs
+++ src/types.rs
@@ -5,9 +5,11 @@ use sp_runtime::{BoundedBTreeMap, BoundedVec};
 pub type MaxParentStyles = ConstU32<32>;
 /// How many sub-styles a parent style can contain.
 pub type MaxSubStyles = ConstU32<32>;
+/// The maximum length of a style name.
+pub type MaxNameLength = ConstU32<32>;
 
 /// A music style name, with a fixed size of maximum 32 characters.
-pub type StyleName = BoundedVec<u8, ConstU32<32>>;
+pub type StyleName = BoundedVec<u8, MaxNameLength>;
 /// A list containing a maximum number of style name, representing some sub-styles.
 pub type SubStyles = BoundedVec<StyleName, MaxSubStyles>;
 /// A fixed size register containing some style name as parent styles, linked to a list of sub-styles.
