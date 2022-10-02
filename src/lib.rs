90bed2e7dd3556ebffb4303d3c14ffb60a07968e
SailorSnoW <snxwin@pm.me>
2022-10-02 13:38:02 +0200

    add functions to MusicStylesProvider and removed required trait for types

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,22 @@ pub mod types;
 
 pub trait MusicStylesProvider {
     /// Type of a style name.
-    type StyleName: Encode + Decode;
+    type StyleName;
     /// Type of a register containing all the styles.
-    type Styles: Encode + Decode;
+    type Styles;
 
     /// Get the full register containing styles and sub-styles.
     fn styles() -> Self::Styles;
     /// Get all the parent_styles contained in the register.
     fn parent_styles() -> Vec<Self::StyleName>;
+    /// Get all the sub-styles contained in the register.
+    fn sub_styles() -> Vec<Self::StyleName>;
     /// Verify that the given style name exist.
     fn exist(style_name: &Self::StyleName) -> bool;
+    /// Verify that the given style name exist and is a parent style.
+    fn is_parent_style(style_name: &Self::StyleName) -> bool;
+    /// Verify that the given style name exist and is a sub-style.
+    fn is_sub_style(style_name: &Self::StyleName) -> bool;
     /// Convert the given value into a style name and check if it exist, returning None if not.
     /// Throw an Error if the value can't be converted.
     fn exist_from<T: TryInto<Self::StyleName>>(
