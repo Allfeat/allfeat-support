5e9e8c7d41918446c1427a9133c3d88cddd41501
SailorSnoW <snxwin@pm.me>
2022-10-03 02:03:41 +0200

    code re-organization, package metadata update, type refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,9 @@
-use sp_runtime::DispatchResult;
+#![cfg_attr(not(feature = "std"), no_std)]
 
+pub mod traits;
 pub mod types;
 
-pub trait InspectMusicStyles {
-    /// Type of a style name.
-    type StyleName;
-    /// Type of a register containing all the styles.
-    type Styles;
-
-    /// Get the full register containing styles and sub-styles.
-    fn styles() -> Self::Styles;
-    /// Get all the parent_styles contained in the register.
-    fn parent_styles() -> Vec<Self::StyleName>;
-    /// Get all the sub-styles contained in the register.
-    fn sub_styles() -> Vec<Self::StyleName>;
-    /// Verify that the given style name exist.
-    fn exist(style_name: &Self::StyleName) -> bool;
-    /// Verify that the given style name exist and is a parent style.
-    fn is_parent_style(style_name: &Self::StyleName) -> bool;
-    /// Verify that the given style name exist and is a sub-style.
-    fn is_sub_style(style_name: &Self::StyleName) -> bool;
-    /// Convert the given value into a style name and check if it exist, returning None if not.
-    /// Throw an Error if the value can't be converted.
-    fn exist_from<T: TryInto<Self::StyleName>>(
-        data: T,
-    ) -> Result<Option<Self::StyleName>, T::Error> {
-        let style_name = data.try_into()?;
-
-        if Self::exist(&style_name) {
-            Ok(Some(style_name))
-        } else {
-            Ok(None)
-        }
-    }
-}
-
-pub trait MutateMusicStyles {
-    /// Type of a style name.
-    type StyleName;
-
-    /// Add a parent style into the database.
-    fn add_parent_style(style_name: Self::StyleName) -> DispatchResult;
-    /// Add a sub-style into the database.
-    fn add_sub_style(
-        sub_style_name: Self::StyleName,
-        parent_style: Self::StyleName,
-    ) -> DispatchResult;
+pub mod prelude {
+    pub use crate::traits::music::style::InspectMusicStyles;
+    pub use crate::types::music::style::{MusicStyleDB, MusicStyleName};
 }
--- src/types/music/style.rs
+++ src/types/music/style.rs
@@ -9,8 +9,8 @@ pub type MaxSubStyles = ConstU32<32>;
 pub type MaxNameLength = ConstU32<32>;
 
 /// A music style name, with a fixed size of maximum 32 characters.
-pub type StyleName = BoundedVec<u8, MaxNameLength>;
+pub type MusicStyleName = BoundedVec<u8, MaxNameLength>;
 /// A list containing a maximum number of style name, representing some sub-styles.
-pub type SubStyles = BoundedVec<StyleName, MaxSubStyles>;
-/// A fixed size register containing some style name as parent styles, linked to a list of sub-styles.
-pub type Styles = BoundedBTreeMap<StyleName, SubStyles, MaxParentStyles>;
+pub type MusicSubStyles = BoundedVec<MusicStyleName, MaxSubStyles>;
+/// A fixed size database containing some style names as parent styles, linked to a list of sub-styles.
+pub type MusicStyleDB = BoundedBTreeMap<MusicStyleName, MusicSubStyles, MaxParentStyles>;
