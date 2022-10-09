43b10b5a5f1a012101879a62f9d115fbdf207be0
SailorSnoW <snxwin@pm.me>
2022-10-10 01:14:33 +0200

    add check_owner param to seal fn

--- src/traits/tokens/nonfungibles.rs
+++ src/traits/tokens/nonfungibles.rs
@@ -1,9 +1,12 @@
-use frame_support::traits::tokens::nonfungibles::Mutate;
+pub use frame_support::traits::tokens::nonfungibles::{Inspect, Mutate};
 use sp_runtime::DispatchResult;
 
 /// Trait for providing an interface for sealing a collection of NFT which may be
 /// attributes set on them and that we want to be sure that these attributes aren't
 /// gonna be modified in the future..
 pub trait Seal<AccountId>: Mutate<AccountId> {
-    fn seal(asset_id: &Self::CollectionId) -> DispatchResult;
+    fn seal(
+        collection: &Self::CollectionId,
+        maybe_check_owner: Option<AccountId>,
+    ) -> DispatchResult;
 }
