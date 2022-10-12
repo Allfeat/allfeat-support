ef3c85d8f060dd4ba0308e6b8309c780115e4fd0
SailorSnoW <snxwin@pm.me>
2022-10-12 23:57:19 +0200

    removed caller id from parameter

--- src/traits/tokens/nonfungibles.rs
+++ src/traits/tokens/nonfungibles.rs
@@ -1,12 +1,16 @@
-pub use frame_support::traits::tokens::nonfungibles::{Inspect, Mutate};
+pub use frame_support::traits::tokens::nonfungibles::{self, Inspect, Mutate};
 use sp_runtime::DispatchResult;
 
 /// Trait for providing an interface for sealing a collection of NFT which may be
 /// attributes set on them and that we want to be sure that these attributes aren't
 /// gonna be modified in the future..
 pub trait Seal<AccountId>: Mutate<AccountId> {
-    fn seal(
+    fn seal(collection: &Self::CollectionId) -> DispatchResult;
+}
+
+pub trait MutateExtend<AccountId>: Mutate<AccountId> {
+    fn set_collection_max_supply(
         collection: &Self::CollectionId,
-        maybe_check_owner: Option<AccountId>,
+        max_supply: u32,
     ) -> DispatchResult;
 }
