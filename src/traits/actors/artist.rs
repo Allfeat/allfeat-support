7ca6a1ebf63ed2f54bfe3b993e063e99b5508f97
SailorSnoW <snxwin@pm.me>
2023-01-10 01:42:24 +0100

    replaced Result with Option

--- src/traits/actors/artist.rs
+++ src/traits/actors/artist.rs
@@ -1,16 +1,13 @@
 use super::ActorStorage;
-use frame_support::traits::PalletError;
 
 pub trait ArtistStorage<T, C, A>: ActorStorage<T> {
-    type Err: PalletError;
-
     /// Return if the specified account is actually a candidate
     fn is_candidate(account_id: &T) -> bool;
     /// Return if the specified account is actually an artist
     fn is_artist(account_id: &T) -> bool;
 
     /// Retrieve the candidate informations of a specified account
-    fn candidate(account_id: &T) -> Result<C, Self::Err>;
+    fn candidate(account_id: &T) -> Option<C>;
     /// Retrieve the candidate informations of a specified account
-    fn artist(account_id: &T) -> Result<A, Self::Err>;
+    fn artist(account_id: &T) -> Option<A>;
 }
