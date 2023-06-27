1f6decb47381e9c50353d3cb76c48bd249b3d459
SailorSnoW <sailorsnow@pm.me>
2023-06-27 16:57:50 +0200

    add origins and Ensure types for contract origins

--- src/types/origins.rs
+++ src/types/origins.rs
@@ -1,9 +1,10 @@
-use allfeat_primitives::AccountId;
+use codec::{Decode, Encode, MaxEncodedLen};
 use core::marker::PhantomData;
 use frame_support::traits::EnsureOrigin;
+use scale_info::TypeInfo;
 
 /// Origin for the chain extension calls.
-#[derive(PartialEq, Eq, Clone, RuntimeDebug, Encode, Decode, TypeInfo, MaxEncodedLen)]
+#[derive(PartialEq, Eq, Clone, Encode, Decode, TypeInfo, MaxEncodedLen)]
 #[codec(mel_bound(AccountId: MaxEncodedLen))]
 pub enum RawChainExtOrigin<AccountId> {
     /// It has been condoned by the contract caller.
@@ -12,7 +13,7 @@ pub enum RawChainExtOrigin<AccountId> {
     Contract(AccountId),
 }
 
-/// Ensure that the origin is from a contract
+/// Ensure that the origin is from the contract of a contract call
 pub struct EnsureContract<AccountId>(PhantomData<AccountId>);
 impl<
         O: Into<Result<RawChainExtOrigin<AccountId>, O>> + From<RawChainExtOrigin<AccountId>>,
@@ -34,5 +35,30 @@ impl<
             AccountId::decode(&mut sp_runtime::traits::TrailingZeroInput::zeroes())
                 .expect("infinite length input; no invalid inputs for type; qed");
         Ok(O::from(RawChainExtOrigin::Contract(zero_account_id)))
+    }
+}
+
+/// Ensure that the origin is from the caller of a contract call
+pub struct EnsureContractCaller<AccountId>(PhantomData<AccountId>);
+impl<
+        O: Into<Result<RawChainExtOrigin<AccountId>, O>> + From<RawChainExtOrigin<AccountId>>,
+        AccountId: Decode,
+    > EnsureOrigin<O> for EnsureContractCaller<AccountId>
+{
+    type Success = AccountId;
+
+    fn try_origin(o: O) -> Result<Self::Success, O> {
+        o.into().and_then(|o| match o {
+            RawChainExtOrigin::Caller(id) => Ok(id),
+            _ => Err(O::from(o)),
+        })
+    }
+
+    #[cfg(feature = "runtime-benchmarks")]
+    fn try_successful_origin() -> Result<O, ()> {
+        let zero_account_id =
+            AccountId::decode(&mut sp_runtime::traits::TrailingZeroInput::zeroes())
+                .expect("infinite length input; no invalid inputs for type; qed");
+        Ok(O::from(RawChainExtOrigin::Caller(zero_account_id)))
     }
 }
